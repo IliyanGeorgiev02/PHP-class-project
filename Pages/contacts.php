fdb2675dc0a6d559dae609fa773f550c5024ddea
IliyanGeorgiev02 <120729244+IliyanGeorgiev02@users.noreply.github.com>
2024-10-06 13:16:15 +0300

    added first pages and features

---
+++ Pages/contacts.php
@@ -0,0 +1,19 @@
+<?php
+// страница контакти
+?>
+<h1>Свържете се с нас</h1>
+<form>
+    <div class="mb-3">
+        <label for="name" class="form-label">Имена</label>
+        <input type="text" class="form-control" id="name" placeholder="Your Name">
+    </div>
+    <div class="mb-3">
+        <label for="email" class="form-label">Имейл</label>
+        <input type="email" class="form-control" id="email" placeholder="Your Email">
+    </div>
+    <div class="mb-3">
+        <label for="message" class="form-label">Съобщение</label>
+        <textarea class="form-control" id="message" rows="4"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Изпрати</button>
+</form>
